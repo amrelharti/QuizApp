aee71c688c834bef46ed5aba0ede4a791bec3554
Amr Elharti <ELHARTI_AMR@emsi-edu.ma>
2024-05-05 11:34:05 +0100

    storing and retrieving the questions from firebase

--- app/src/main/java/com/example/quizapp/Question3.java
+++ app/src/main/java/com/example/quizapp/Question3.java
@@ -1,45 +1,91 @@
 package com.example.quizapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Question3 extends AppCompatActivity {
-
     int score;
     Button next;
+    TextView questionText;
     RadioGroup rg;
-    RadioButton rb;
-    String correctAnswer="Drag Reduction System";
+    RadioButton rb, radioA, radioB, radioC;
+    private FirebaseFirestore db;
+    String correctAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question3);
-        rg=findViewById(R.id.rg);
-        next=findViewById(R.id.buttonNext);
-        Intent i=getIntent();
-        score=i.getIntExtra("score",score);
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(rg.getCheckedRadioButtonId()==-1){
-                    Toast.makeText(getApplicationContext(), "select a correct answer", Toast.LENGTH_SHORT).show();
-                }else{
-                    rb=findViewById(rg.getCheckedRadioButtonId());
-                    if(rb.getText().toString().equals(correctAnswer))
-                        score+=1;
-                    Intent i1=new Intent(getApplicationContext(),Question4.class);
-                    i1.putExtra("score",score);
-                    startActivity(i1);
-                    finish();
+
+        rg = findViewById(R.id.rg);
+        next = findViewById(R.id.buttonNext3);
+        questionText = findViewById(R.id.question_text3);
+        radioA = findViewById(R.id.radioA3);
+        radioB = findViewById(R.id.radioB3);
+        radioC = findViewById(R.id.radioC3);
+
+        Intent intent = getIntent();
+        score = intent.getIntExtra("score", 0);
+
+        db = FirebaseFirestore.getInstance();
+
+        db.collection("questions").document("PEMnZiOr9TxHoqoB2wzt")
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        String question = documentSnapshot.getString("question");
+                        String optionA = documentSnapshot.getString("option_a");
+                        String optionB = documentSnapshot.getString("option_b");
+                        String optionC = documentSnapshot.getString("option_c");
+                        correctAnswer = documentSnapshot.getString("correct_answer");
+
+                        questionText.setText(question);
+                        radioA.setText(optionA);
+                        radioB.setText(optionB);
+                        radioC.setText(optionC);
+
+                        // Log the data
+                        Log.d("Question3", "Question: " + question);
+                        Log.d("Question1", "Option A: " + optionA);
+                        Log.d("Question1", "Option B: " + optionB);
+                        Log.d("Question1", "Option C: " + optionC);
+                        Log.d("Question1", "Correct Answer: " + correctAnswer);
+                    } else {
+                        Log.d("Question3", "No such document");
+                        Toast.makeText(getApplicationContext(), "No such document", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    Log.e("Question3", "Error getting document", e);
+                    Toast.makeText(getApplicationContext(), "Error getting document", Toast.LENGTH_SHORT).show();
+                });
+
+        next.setOnClickListener(v -> {
+            if (rg.getCheckedRadioButtonId() == -1) {
+                Toast.makeText(getApplicationContext(), "Select an answer", Toast.LENGTH_SHORT).show();
+            } else {
+                rb = findViewById(rg.getCheckedRadioButtonId());
+                String selectedAnswer = rb.getText().toString();
+                if (selectedAnswer.equals(correctAnswer)) {
+                    score += 1;
+                    Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Incorrect!", Toast.LENGTH_SHORT).show();
                 }
+
+                Intent i1 = new Intent(getApplicationContext(), Question4.class);
+                i1.putExtra("score", score);
+                startActivity(i1);
+                finish();
             }
         });
-    }}
+    }
+}
--- app/src/main/java/com/example/quizapp/Question5.java
+++ app/src/main/java/com/example/quizapp/Question5.java
@@ -1,45 +1,91 @@
 package com.example.quizapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Question5 extends AppCompatActivity {
-
     int score;
     Button next;
+    TextView questionText;
     RadioGroup rg;
-    RadioButton rb;
-    String correctAnswer="Soft tyres";
+    RadioButton rb, radioA, radioB, radioC;
+    private FirebaseFirestore db;
+    String correctAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question5);
-        rg=findViewById(R.id.rg);
-        next=findViewById(R.id.buttonNext);
-        Intent i=getIntent();
-        score=i.getIntExtra("score",score);
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(rg.getCheckedRadioButtonId()==-1){
-                    Toast.makeText(getApplicationContext(), "select a correct answer", Toast.LENGTH_SHORT).show();
-                }else{
-                    rb=findViewById(rg.getCheckedRadioButtonId());
-                    if(rb.getText().toString().equals(correctAnswer))
-                        score+=1;
-                    Intent i1=new Intent(getApplicationContext(),Score.class);
-                    i1.putExtra("score",score);
-                    startActivity(i1);
-                    finish();
+
+        rg = findViewById(R.id.rg);
+        next = findViewById(R.id.buttonNext5);
+        questionText = findViewById(R.id.question_text5);
+        radioA = findViewById(R.id.radioA5);
+        radioB = findViewById(R.id.radioB5);
+        radioC = findViewById(R.id.radioC5);
+
+        Intent intent = getIntent();
+        score = intent.getIntExtra("score", 0);
+
+        db = FirebaseFirestore.getInstance();
+
+        db.collection("questions").document("vkGXHxOR1hP3bEpcgmSn")
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        String question = documentSnapshot.getString("question");
+                        String optionA = documentSnapshot.getString("option_a");
+                        String optionB = documentSnapshot.getString("option_b");
+                        String optionC = documentSnapshot.getString("option_c");
+                        correctAnswer = documentSnapshot.getString("correct_answer");
+
+                        questionText.setText(question);
+                        radioA.setText(optionA);
+                        radioB.setText(optionB);
+                        radioC.setText(optionC);
+
+                        // Log the data
+                        Log.d("Question5", "Question: " + question);
+                        Log.d("Question1", "Option A: " + optionA);
+                        Log.d("Question1", "Option B: " + optionB);
+                        Log.d("Question1", "Option C: " + optionC);
+                        Log.d("Question1", "Correct Answer: " + correctAnswer);
+                    } else {
+                        Log.d("Question5", "No such document");
+                        Toast.makeText(getApplicationContext(), "No such document", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    Log.e("Question5", "Error getting document", e);
+                    Toast.makeText(getApplicationContext(), "Error getting document", Toast.LENGTH_SHORT).show();
+                });
+
+        next.setOnClickListener(v -> {
+            if (rg.getCheckedRadioButtonId() == -1) {
+                Toast.makeText(getApplicationContext(), "Select an answer", Toast.LENGTH_SHORT).show();
+            } else {
+                rb = findViewById(rg.getCheckedRadioButtonId());
+                String selectedAnswer = rb.getText().toString();
+                if (selectedAnswer.equals(correctAnswer)) {
+                    score += 1;
+                    Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Incorrect!", Toast.LENGTH_SHORT).show();
                 }
+
+                Intent i1 = new Intent(getApplicationContext(), Score.class);
+                i1.putExtra("score", score);
+                startActivity(i1);
+                finish();
             }
         });
-    }}
+    }
+}
